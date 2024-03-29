7503b9cf04dbc9fd30c7ab8af6d8277d02831a35
Hieu <Hieu2k2boss@gmail.com>
2024-03-29 17:19:48 +0700

    add AI

---
+++ DQL/src/eval_main.py
@@ -0,0 +1,357 @@
+import numpy as np
+import torch
+
+import agents
+import model
+import utils
+
+# env_small: 9x9, env_regular: 15x15
+from env import env_small as game
+
+#WebAPI
+import logging
+import threading
+import flask
+from WebAPI import web_api
+from WebAPI import game_info
+from WebAPI import player_agent_info
+from WebAPI import enemy_agent_info
+from info.agent_info import AgentInfo
+from info.game_info import GameInfo
+
+BOARD_SIZE = game.Return_BoardParams()[0]
+
+N_BLOCKS_PLAYER = 10
+N_BLOCKS_ENEMY = 10
+
+IN_PLANES_PLAYER = 5  # history * 2 + 1
+IN_PLANES_ENEMY = 5
+
+OUT_PLANES_PLAYER = 128
+OUT_PLANES_ENEMY = 128
+
+N_MCTS = 400
+N_MATCH = 3
+
+use_cuda = torch.cuda.is_available()
+device = torch.device('cuda' if use_cuda else 'cpu')
+
+# ==================== input model path ================= #
+#       'human': human play       'puct': PUCB MCTS       #
+#       'uct': UCB MCTS           'random': random        #
+#       'web': web play                                   #
+# ======================================================= #
+# example)
+
+player_model_path = 'web'
+enemy_model_path = './data/180828_8300_235156_step_model.pickle'
+monitor_model_path = './data/180828_8300_235156_step_model.pickle'
+
+class Evaluator(object):
+    def __init__(self):
+        self.player = None
+        self.enemy = None
+        self.monitor = None
+        pass
+
+    def set_agents(self, model_path_a, model_path_b, model_path_m):
+
+        if model_path_a == 'human' or model_path_b == 'human':
+            game_mode = 'pygame'
+        else:
+            game_mode = 'text'
+
+        self.env = game.GameState(game_mode)
+
+        if model_path_a == 'random':
+            print('load player model:', model_path_a)
+            self.player = agents.RandomAgent(BOARD_SIZE)
+        elif model_path_a == 'puct':
+            print('load player model:', model_path_a)
+            self.player = agents.PUCTAgent(BOARD_SIZE, N_MCTS)
+        elif model_path_a == 'uct':
+            print('load player model:', model_path_a)
+            self.player = agents.UCTAgent(BOARD_SIZE, N_MCTS)
+        elif model_path_a == 'human':
+            print('load player model:', model_path_a)
+            self.player = agents.HumanAgent(BOARD_SIZE, self.env)
+        elif model_path_a == 'web':
+            print('load player model:', model_path_a)
+            self.player = agents.WebAgent(BOARD_SIZE)
+        else:
+            print('load player model:', model_path_a)
+            self.player = agents.ZeroAgent(BOARD_SIZE,
+                                           N_MCTS,
+                                           IN_PLANES_PLAYER,
+                                           noise=False)
+            self.player.model = model.PVNet(N_BLOCKS_PLAYER,
+                                            IN_PLANES_PLAYER,
+                                            OUT_PLANES_PLAYER,
+                                            BOARD_SIZE).to(device)
+            state_a = self.player.model.state_dict()
+            my_state_a = torch.load(
+                model_path_a, map_location='cuda:0' if use_cuda else 'cpu')
+            for k, v in my_state_a.items():
+                if k in state_a:
+                    state_a[k] = v
+            self.player.model.load_state_dict(state_a)
+
+        if model_path_b == 'random':
+            print('load enemy model:', model_path_b)
+            self.enemy = agents.RandomAgent(BOARD_SIZE)
+        elif model_path_b == 'puct':
+            print('load enemy model:', model_path_b)
+            self.enemy = agents.PUCTAgent(BOARD_SIZE, N_MCTS)
+        elif model_path_b == 'uct':
+            print('load enemy model:', model_path_b)
+            self.enemy = agents.UCTAgent(BOARD_SIZE, N_MCTS)
+        elif model_path_b == 'human':
+            print('load enemy model:', model_path_b)
+            self.enemy = agents.HumanAgent(BOARD_SIZE, self.env)
+        elif model_path_b == 'web':
+            print('load enemy model:', model_path_b)
+            self.enemy = agents.WebAgent(BOARD_SIZE)
+        else:
+            print('load enemy model:', model_path_b)
+            self.enemy = agents.ZeroAgent(BOARD_SIZE,
+                                          N_MCTS,
+                                          IN_PLANES_ENEMY,
+                                          noise=False)
+            self.enemy.model = model.PVNet(N_BLOCKS_ENEMY,
+                                           IN_PLANES_ENEMY,
+                                           OUT_PLANES_ENEMY,
+                                           BOARD_SIZE).to(device)
+            state_b = self.enemy.model.state_dict()
+            my_state_b = torch.load(
+                model_path_b, map_location='cuda:0' if use_cuda else 'cpu')
+            for k, v in my_state_b.items():
+                if k in state_b:
+                    state_b[k] = v
+            self.enemy.model.load_state_dict(state_b)
+
+        #monitor agent
+        self.monitor = agents.ZeroAgent(BOARD_SIZE,
+                                        10, #N_MCTS
+                                        IN_PLANES_ENEMY,
+                                        noise=False)
+        self.monitor.model = model.PVNet(N_BLOCKS_ENEMY,
+                                        IN_PLANES_ENEMY,
+                                        OUT_PLANES_ENEMY,
+                                        BOARD_SIZE).to(device)
+        state_b = self.monitor.model.state_dict()
+        my_state_b = torch.load(
+            model_path_m, map_location='cuda:0' if use_cuda else 'cpu')
+        for k, v in my_state_b.items():
+            if k in state_b:
+                state_b[k] = v
+        self.monitor.model.load_state_dict(state_b)            
+
+    def get_action(self, root_id, board, turn, enemy_turn):
+        if turn != enemy_turn:
+            if isinstance(self.player, agents.ZeroAgent):
+                pi = self.player.get_pi(root_id, tau=0)
+            else:
+                pi = self.player.get_pi(root_id, board, turn, tau=0)
+
+                # for monitor
+                self.monitor.get_pi(root_id, tau=0)
+        else:
+            if isinstance(self.enemy, agents.ZeroAgent):
+                pi = self.enemy.get_pi(root_id, tau=0)
+            else:
+                pi = self.enemy.get_pi(root_id, board, turn, tau=0)
+
+        action, action_index = utils.argmax_onehot(pi)
+
+        return action, action_index
+
+    def return_env(self):
+        return self.env
+
+    def reset(self):
+        self.player.reset()
+        self.enemy.reset()
+
+    def put_action(self, action_idx, turn, enemy_turn):
+        
+        print(self.player)
+
+        if turn != enemy_turn:
+            if type(self.player) is agents.WebAgent:
+                self.player.put_action(action_idx)
+        else:
+            if type(self.enemy) is agents.WebAgent:
+                self.enemy.put_action(action_idx)
+
+def elo(player_elo, enemy_elo, p_winscore, e_winscore):
+    elo_diff = enemy_elo - player_elo
+    ex_pw = 1 / (1 + 10**(elo_diff / 400))
+    ex_ew = 1 / (1 + 10**(-elo_diff / 400))
+    player_elo += 32 * (p_winscore - ex_pw)
+    enemy_elo += 32 * (e_winscore - ex_ew)
+
+    return player_elo, enemy_elo
+
+evaluator = Evaluator()
+
+def main():
+    evaluator.set_agents(player_model_path, enemy_model_path, monitor_model_path)
+ 
+    player_agent_info.agent = evaluator.player
+    enemy_agent_info.agent = evaluator.enemy
+
+    env = evaluator.return_env()
+
+    result = {'Player': 0, 'Enemy': 0, 'Draw': 0}
+    turn = 0
+    enemy_turn = 1
+    player_elo = 1500
+    enemy_elo = 1500
+
+    game_info.enemy_turn = enemy_turn
+    game_info.game_status = 0
+
+    print('Player ELO: {:.0f}, Enemy ELO: {:.0f}'.format(
+        player_elo, enemy_elo))
+
+    for i in range(N_MATCH):
+        board = np.zeros([BOARD_SIZE, BOARD_SIZE])
+        root_id = (0,)
+        win_index = 0
+        action_index = None
+
+        game_info.game_board = board
+
+        if i % 2 == 0:
+            print('Player Color: Black')
+        else:
+            print('Player Color: White')
+
+        game_info.game_status = 0 #0:Running 1:Player Win, 2: Enemy Win 3: Draw
+
+        while win_index == 0:
+            utils.render_str(board, BOARD_SIZE, action_index)
+
+            p, v = evaluator.monitor.get_pv(root_id)
+
+            action, action_index = evaluator.get_action(root_id,
+                                                        board,
+                                                        turn,
+                                                        enemy_turn)
+
+            if turn != enemy_turn:
+                # player turn
+                root_id = evaluator.player.root_id + (action_index,)
+            else:
+                # enemy turn
+                root_id = evaluator.enemy.root_id + (action_index,)
+
+            board, check_valid_pos, win_index, turn, _ = env.step(action)
+
+            game_info.game_board = board
+            game_info.action_index = int(action_index)
+            game_info.win_index = win_index
+            game_info.curr_turn = turn # 0 black 1 white  
+            
+            move = np.count_nonzero(board)
+
+            if turn == enemy_turn:
+                
+                if isinstance(evaluator.player, agents.HumanAgent) or isinstance(evaluator.player, agents.WebAgent):
+                    player_agent_info.visit = evaluator.monitor.get_visit()
+                    player_agent_info.p = evaluator.monitor.get_policy()
+                else:
+                    player_agent_info.visit = evaluator.player.get_visit()
+                    player_agent_info.p = evaluator.player.get_policy()   
+
+                player_agent_info.add_value(move, v)                         
+                evaluator.enemy.del_parents(root_id)
+
+            else:
+                enemy_agent_info.visit = evaluator.enemy.get_visit()
+                enemy_agent_info.p = evaluator.enemy.get_policy()
+                enemy_agent_info.add_value(move, v)                
+                evaluator.player.del_parents(root_id)                
+
+            if win_index != 0:
+                player_agent_info.clear_values()
+                enemy_agent_info.clear_values()
+
+                game_info.game_status = win_index # 0:Running 1:Player Win, 2: Enemy Win 3: Draw
+
+                if turn == enemy_turn:
+                    if win_index == 3:
+                        result['Draw'] += 1
+                        print('\nDraw!')
+                        player_elo, enemy_elo = elo(
+                            player_elo, enemy_elo, 0.5, 0.5)
+                    else:
+                        result['Player'] += 1
+                        print('\nPlayer Win!')
+                        player_elo, enemy_elo = elo(
+                            player_elo, enemy_elo, 1, 0)
+                else:
+                    if win_index == 3:
+                        result['Draw'] += 1
+                        print('\nDraw!')
+                        player_elo, enemy_elo = elo(
+                            player_elo, enemy_elo, 0.5, 0.5)
+                    else:
+                        result['Enemy'] += 1
+                        print('\nEnemy Win!')
+                        player_elo, enemy_elo = elo(
+                            player_elo, enemy_elo, 0, 1)
+
+                utils.render_str(board, BOARD_SIZE, action_index)
+                # Change turn
+                enemy_turn = abs(enemy_turn - 1)
+                turn = 0
+
+                game_info.enemy_turn = enemy_turn
+                game_info.curr_turn = turn
+
+                pw, ew, dr = result['Player'], result['Enemy'], result['Draw']
+                winrate = (pw + 0.5 * dr) / (pw + ew + dr) * 100
+                print('')
+                print('=' * 20, " {}  Game End  ".format(i + 1), '=' * 20)
+                print('Player Win: {}'
+                      '  Enemy Win: {}'
+                      '  Draw: {}'
+                      '  Winrate: {:.2f}%'.format(
+                          pw, ew, dr, winrate))
+                print('Player ELO: {:.0f}, Enemy ELO: {:.0f}'.format(
+                    player_elo, enemy_elo))
+                evaluator.reset()
+
+# WebAPI
+app = flask.Flask(__name__)
+app.register_blueprint(web_api)
+log = logging.getLogger('werkzeug')
+log.disabled = True
+
+@app.route('/action')
+def action():
+
+    action_idx = int(flask.request.args.get("action_idx"))
+    data = {"success": False}
+    evaluator.put_action(action_idx, game_info.curr_turn, game_info.enemy_turn)
+    data["success"] = True
+
+    return flask.jsonify(data)
+
+if __name__ == '__main__':
+    print('cuda:', use_cuda)
+    np.set_printoptions(suppress=True)
+    np.random.seed(0)
+    torch.manual_seed(0)
+    if use_cuda:
+        torch.cuda.manual_seed_all(0)
+
+    # WebAPI
+    print("Activate WebAPI...")
+    app_th = threading.Thread(target=app.run,
+                              kwargs={"host": "0.0.0.0", "port": 5000})
+    app_th.start()
+    main()
+
+
---
+++ DQL/src/static/gameboard.js
@@ -0,0 +1,484 @@
+var baseurl = "http://127.0.0.1:5000/"
+
+var c = document.getElementById("board");
+var ctx = c.getContext("2d");
+
+var game_board_size = 9;
+var radius = 14;
+var blank = 22;
+var turn = 1; // 1 black 2 white
+var prev_turn = -1;
+var width = (game_board_size - 1) * 32 + blank * 2;
+var height = (game_board_size - 1) * 32 + blank * 2;
+
+var selecting_board_row = -1;
+var selecting_board_col = -1;
+
+var selected_board_row = -1;
+var selected_board_col = -1;
+
+var select_player_agent_name = document.getElementById('select_player_agent_name').options[0];
+var select_enemy_agent_name = document.getElementById('select_enemy_agent_name').options[0];
+
+var player_message = document.getElementById("player_message");
+var enemy_message = document.getElementById("enemy_message");
+
+var boardArray = new Array(game_board_size); 
+for (var i = 0; i < game_board_size; i++) {
+    boardArray[i] = new Array(game_board_size);
+    for (j = 0; j < game_board_size; j++) { 
+		boardArray[i][j] = 0;
+	}
+}
+
+/* Mouse Event */
+function getBoardPos(canvas, evt) {
+	var rect = canvas.getBoundingClientRect();
+	
+	var xPos = evt.clientX - rect.left;
+	var yPos = evt.clientY - rect.top;
+
+
+	var xIdx = (xPos - blank) / 32;
+	var resultX = Math.round(xIdx);
+	var yIdx = (yPos - blank) / 32;
+	var resultY = Math.round(yIdx);
+
+	return {
+		x: resultX,
+		y: resultY
+	};
+}
+
+c.addEventListener('mousemove', function(evt) 
+{
+
+	var pos = getBoardPos(c, evt);
+
+	selecting_board_row = pos.y;
+	selecting_board_col = pos.x;
+
+	renderBoard();
+
+}, false);
+
+c.addEventListener('mousedown', function(evt) 
+{
+	var pos = getBoardPos(c, evt);
+
+    if (boardArray[pos.x][pos.y] == 0)
+    {
+        selected_board_row = pos.y;
+        selected_board_col = pos.x;
+    }
+
+	renderBoard();
+
+}, false);
+
+var headmap_color_blues = [
+	"#f7fbff",
+	"#deebf7",
+	"#c6dbef",
+	"#9ecae1",
+	"#6baed6",
+	"#4292c6",
+	"#2171b5",
+	"#08519c",
+	"#08306b"
+];
+
+var headmap_color_greens = [
+	"#f7fcf5",
+	"#e5f5e0",
+	"#c7e9c0",
+	"#a1d99b",
+	"#74c476",
+	"#41ab5d",
+	"#238b45",
+	"#006d2c",
+	"#00441b"
+];
+
+var heatmap_color_grays = [
+	"#ffffff",
+	"#f0f0f0",
+	"#d9d9d9",
+	"#bdbdbd",
+	"#969696",
+	"#737373",
+	"#525252",
+	"#252525",
+	"#000000"
+];
+
+var heatmap_color_oranges = [
+	"#fff5eb",
+	"#fee6ce",
+	"#fdd0a2",
+	"#fdae6b",
+	"#fd8d3c",
+	"#f16913",
+	"#d94801",
+	"#a63603",
+	"#7f2704"
+];
+
+var heatmap_color_purples = [
+	"#fcfbfd",
+	"#efedf5",
+	"#dadaeb",
+	"#bcbddc",
+	"#9e9ac8",
+	"#807dba",
+	"#6a51a3",
+	"#54278f",
+	"#3f007d"
+];
+
+var heatmap_color_reds = [
+	"#fff5f0",
+	"#fee0d2",
+	"#fcbba1",
+	"#fc9272",
+	"#fb6a4a",
+	"#ef3b2c",
+	"#cb181d",
+	"#a50f15",
+	"#67000d"
+];
+
+var player_move = [];
+var player_value = [];
+var enemy_move = [];
+var enemy_value = [];
+
+var action_index = -1;
+
+function updateBoard(ret)
+{
+    if (ret.curr_turn == 0) // black turn: 0, white turn: 1
+    {
+        turn = 1 // black
+    }
+    else 
+    {
+        turn = 2 // white
+	}
+
+	// if turn change
+	if (prev_turn != turn)
+	{
+		selecting_board_row = -1;
+		selecting_board_col = -1;
+		selected_board_row = -1;
+		selected_board_col = -1;
+
+		prev_turn = turn;
+	}
+	
+	action_index = ret.action_index;
+
+    game_board_size = ret.game_board_size;
+
+    for (var i = 0; i < game_board_size; i++) 
+    {
+        for (j = 0; j < game_board_size; j++) 
+        { 
+            idx = i + j * game_board_size;
+
+            switch (ret.game_board_values[idx])
+            {
+                case 1:
+                    boardArray[i][j] = 1;
+                    break;
+                case -1:
+                    boardArray[i][j] = 2;
+                    break;		
+                case 0:
+                    boardArray[i][j] = 0;
+                    break;		
+            }
+        }
+    }
+}
+
+function updateVPlot(ret)
+{
+    player_move = ret.player_agent_moves;
+    player_value = ret.player_agent_values;
+    enemy_move = ret.enemy_agent_moves;
+    enemy_value = ret.enemy_agent_values;
+}
+
+function renderBoard(){
+
+	// board fill color
+	ctx.fillStyle="#ffcc66";
+	ctx.fillRect(0, 0, width, height);
+
+	// board draw line
+	ctx.strokeStyle = 'black';
+	ctx.fillStyle="#FF0000";
+	ctx.lineWidth = 1
+	for (i = 0; i < game_board_size; i++) { 
+		// horizontal line draw
+		ctx.beginPath();
+		ctx.moveTo(blank + i * 32 + 0.5, blank);
+		ctx.lineTo(blank + i * 32 + 0.5, width - blank + 0.5);
+		ctx.stroke();
+
+		// vertical line draw
+		ctx.beginPath();
+		ctx.moveTo(blank, blank + i * 32 + 0.5);
+		ctx.lineTo(height - blank, blank + i * 32 + 0.5);
+		ctx.stroke();
+	}
+
+	ctx.shadowColor = 'black';
+
+	// console.log('render : ' + turn);
+
+	// board draw
+	for (i = 0; i < game_board_size; i++) { 
+		for (j = 0; j < game_board_size; j++) 
+		{
+			ctx.shadowBlur = 5;
+			ctx.shadowOffsetX = 2;
+			ctx.shadowOffsetY = 2;
+			ctx.shadowColor="#333333";
+
+			if (boardArray[i][j] == 1) {
+				ctx.beginPath();
+				ctx.strokeStyle="#000000";
+				ctx.fillStyle="#000000";
+				ctx.arc(blank + i * 32, blank + j * 32, radius, 0, 2*Math.PI);
+				ctx.fill();
+				//ctx.stroke();
+			} else if (boardArray[i][j] == 2){
+				ctx.beginPath();
+				ctx.strokeStyle="#ffffff";
+				ctx.fillStyle="#ffffff";
+				ctx.arc(blank + i * 32, blank + j * 32, radius, 0, 2*Math.PI);
+				ctx.fill();
+				//ctx.stroke();
+			}
+
+			ctx.shadowOffsetX = 0;
+			ctx.shadowOffsetY = 0;
+			ctx.shadowBlur = 0;
+			
+			ctx.lineWidth = 2;
+
+			if(action_index == (j * game_board_size + i))
+			{
+				if (boardArray[i][j] == 1) {
+					ctx.beginPath();
+					ctx.strokeStyle="#ffffff";
+					ctx.arc(blank + i * 32, blank + j * 32, radius / 2.0, 0, 2*Math.PI);
+					ctx.stroke();
+				} else if (boardArray[i][j] == 2){
+					ctx.beginPath();
+					ctx.strokeStyle="#000000";
+					ctx.arc(blank + i * 32, blank + j * 32, radius / 2.0, 0, 2*Math.PI);
+					ctx.stroke();
+				}
+			}
+
+			if (i == selecting_board_row && j == selecting_board_col)
+			{
+				ctx.lineWidth = 1;
+
+				ctx.beginPath();
+				ctx.globalAlpha=0.8;
+
+				if (turn == 1) // 1 black
+				{
+					ctx.strokeStyle="#000000";
+					ctx.fillStyle="#000000";
+				}
+				else if (turn == 2) // 2 white
+				{
+					ctx.strokeStyle="#ffffff";
+					ctx.fillStyle="#ffffff";	
+				}
+
+				ctx.arc(blank + selecting_board_col * 32, blank + selecting_board_row * 32, radius, 0, 2*Math.PI);
+				ctx.fill();
+				ctx.stroke();
+				ctx.globalAlpha=1;
+			}
+
+			if (i == selected_board_row && j == selected_board_col)
+			{
+				ctx.lineWidth = 5;
+
+				ctx.beginPath();
+
+				if (turn == 1) // 1 black
+				{
+					ctx.strokeStyle="#000000";
+					ctx.fillStyle="#aaaaaa";
+				}
+				else if (turn == 2) // 2 white
+				{
+					ctx.strokeStyle="#ffffff";
+					ctx.fillStyle="#aaaaaa";	
+				}
+
+				ctx.arc(blank + selected_board_col * 32, blank + selected_board_row * 32, radius - 2, 0, 2*Math.PI);
+				ctx.stroke();
+			}
+		}
+    }
+    
+    // board_message.innerHTML = "AlphaOmoc vs AlphaOmoc";
+}
+
+function renderVPlot()
+{
+    var trace1 = {
+        x: player_move, 
+        y: player_value, 
+        type: 'scatter',
+        fill: 'tozeroy',
+        name: 'player'
+    };
+    
+    var trace2 = {
+        x: enemy_move, 
+        y: enemy_value, 
+        type: 'scatter',
+        fill: 'tozeroy',
+        name: 'enemy'
+	};
+	
+	var layout = {
+		title: 'Value Network',
+		xaxis: {
+			range: [0, 81],
+			autorange: false
+		},
+		yaxis: {
+			range: [0.0, 100],
+			autorange: false
+		}
+	};
+
+    var data = [trace1, trace2];
+    Plotly.newPlot('v_monitoring', data, layout);
+}
+
+function reqPeriodicStatus()
+{
+    var xhr = new XMLHttpRequest();
+    
+    xhr.onload = function() 
+    {
+        if(xhr.status == 200) 
+        {
+            ret = JSON.parse(xhr.responseText);
+			
+			select_player_agent_name.value = ret.player_agent_name;
+			select_player_agent_name.text = ret.player_agent_name;
+			select_enemy_agent_name.value = ret.enemy_agent_name;
+			select_enemy_agent_name.text = ret.enemy_agent_name;
+
+			updateBoard(ret);
+            updateVPlot(ret);
+
+            renderBoard();
+            renderVPlot();
+        }
+    };
+
+    xhr.open('GET', baseurl + 'periodic_status', true);
+    xhr.send();
+}
+
+function reqPromptStatus()
+{
+    var xhr = new XMLHttpRequest();
+    
+    xhr.onload = function() 
+    {
+        if(xhr.status == 200) 
+        {
+            ret = JSON.parse(xhr.responseText);
+
+            player_message.innerHTML = 'player : ' + ret.player_message;
+            enemy_message.innerHTML = 'enemy : ' + ret.enemy_message;	
+        }
+    };
+
+    xhr.open('GET', baseurl + 'prompt_status', true);
+    xhr.send();
+}
+
+function reqResetAgents()
+{
+	var xhr = new XMLHttpRequest();
+    
+    xhr.onload = function() 
+    {
+        if(xhr.status == 200) 
+        {
+			document.getElementById("select_player_agent_name").options[0].selected=true;
+			document.getElementById("select_enemy_agent_name").options[0].selected=true;	
+        }
+	};
+	
+	selected_player_agent_name = document.getElementById("select_player_agent_name").value;
+	selected_enemy_agent_name = document.getElementById("select_enemy_agent_name").value;
+
+    xhr.open('GET', baseurl + 'req_reset_agenets?player_agent='+selected_player_agent_name + '&enemy_agent='+selected_enemy_agent_name, true);
+    xhr.send();
+}
+
+function reqMove()
+{
+	if (selected_board_col >= 0 
+		&& selected_board_col < game_board_size 
+		&& selected_board_row >= 0
+		&& selected_board_row < game_board_size 
+		&& boardArray[selected_board_col][selected_board_row] == 0)
+	{
+		action_idx = selected_board_col + selected_board_row * game_board_size;
+
+		reqAction(action_idx)
+	}
+}
+
+function reqAction(action_idx)
+{
+	var xhr = new XMLHttpRequest();
+
+	xhr.onload = function() 
+	{
+		if(xhr.status == 200) 
+		{
+			
+		}
+	};
+
+	xhr.open('GET', baseurl +'action?action_idx=' + action_idx.toString(), true);
+	xhr.send();
+}
+
+function clearBoard()
+{
+	for (var i = 0; i < game_board_size; i++) 
+	{
+		for (j = 0; j < game_board_size; j++) 
+		{ 
+			boardArray[i][j] = 0;
+		}
+	}
+
+}
+
+renderBoard();
+renderVPlot();
+
+setInterval(reqPeriodicStatus, 500);
+setInterval(reqPromptStatus, 100);
